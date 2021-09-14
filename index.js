3c8ebe534ab39857b24fc1d12abd73b5223ebc6e
arnaz-mo <90614170+arnaz-mo@users.noreply.github.com>
2021-09-14 17:54:43 +0530

    adding Unique user id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getInfo() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-    
+    Moengage.add_unique_user_id(email);
 
     console.log(" Thank you " + username + " for logging in ")
 }
@@ -12,20 +12,23 @@ function logout(){
 }
 
 function updateUser(){
-   // Moengage.update_unique_user_id(NEW_UNIQUE_ID);
+    Moengage.update_unique_user_id(email);
 }
 
-Moengage.add_first_name("Dominick");
-Moengage.add_last_name("Cobb");
-Moengage.add_email("dom@level5.com");
-Moengage.add_mobile("+12399999999");
-Moengage.add_user_name("Dominick (Dom) Cobb"); // Full name for user
-Moengage.add_gender("M");
-Moengage.add_birthday(new Date(1980, 2, 31));
 
-Moengage.track_event("Login Actions", {
-    "name": "Arnaz", // string value
-    "Password": 0, // numeric value
-    "email id": "arnazshaikh@gmail.com", // numeric value
-    "date": new Date(2017, 0, 31), // datetime value. Example value represents 31 January, 2017.
-    });
+function bookNow()
+{
+	Moengage.track_event("Book_Now",
+      {
+        "prid": 388303,
+        "brand": "Kaya youth",
+        "prname": "Face Polishing Scrub |8901088155120",
+        "PrMrp": 325,
+        "Price": 97,
+        "discount": 70,
+        "quantity": 1,
+        "category": "Dull Skin",
+        "category_id": "59754"
+       
+      });	
+}
